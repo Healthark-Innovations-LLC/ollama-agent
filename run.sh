f55ce275fb0cc40f2674606d826b528ce42d725d
Eric <58240560+naisanzaa@users.noreply.github.com>
2026-06-08 01:58:19 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+# run ollama in docker
+
+set -xe
+
+docker rm -f ollama
+
+docker run -d \
+  --restart always \
+  --name ollama \
+  --gpus=all \
+  -v ollama:/root/.ollama \
+  -p 11434:11434 \
+  ollama/ollama
+
+docker ps | grep ollama
